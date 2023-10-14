b14808cd33ab4e11860810b2bb03c651ff384f20
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-10-14 13:48:20 +0200

    Add Obsolet Components & Video 4 Midudev

--- bootcamp/src/Components/App.js
+++ bootcamp/src/Components/App.js
@@ -1,13 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// import axios from 'axios';
 import './App.css';
 import Note from './Note';
 
 function App(props) {
 
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
-  
+
+  // Opcio Fetch
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then((json) => {
+        setNotes(json)
+      });
+  }, [])
+
+  // // Opcio Axios (cal importar "npm install axios")
+  // useEffect(() => {
+  //   axios.get('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => {
+  //       const {data} = response
+  //       setNotes(data)
+  //     });
+  // }, [])
+
   const handleChange = (event) => {
     setNewNote(event.target.value);
   }
@@ -16,44 +34,27 @@ function App(props) {
     event.preventDefault();
     const noteToAddToState = {
       id: notes.length + 1,
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
+      title: newNote,
+      body: newNote,
     }
     setNotes([...notes, noteToAddToState])
     setNewNote('');
   }
 
-  const handleShowAll = () => {
-    setShowAll(() => !showAll)
-  }
-
   return (
-    typeof notes === "undefined" ? "No hay notas a mostrar" : 
-    notes.length === 0 ? "No tenemos notas a mostrar" : 
     <div>
       <h1>Notes</h1>
-      <button onClick={handleShowAll}>
-        {showAll ? "Show only importa" : "Show all"}
-      </button>
       <ol>
-        {notes
-        .filter((notes) => {
-          if (showAll === true) return true;
-          return notes.important === true
-        } 
-        
-        )
-        .map((notes) => (
-          <Note key={notes.id} id={notes.id} content={notes.content} date={notes.date}/>
+        {notes.length === 0 ? "Cargando notas" :
+          notes.map((notes) => (
+          <Note key={notes.id} {...notes} />
         ))}
       </ol>
-        <form onSubmit={handleSubmit}>
-          <input type='text' onChange={handleChange} value={newNote}></input>
-          <button type='submit'>Crear nota</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} value={newNote}></input>
+        <button type='submit'>Crear nota</button>
+      </form>
     </div>
-    
   )    
 }
 
--- bootcamp/src/Components/Note.js
+++ bootcamp/src/Components/Note.js
@@ -1,22 +1,10 @@
-
-const Note = ({id, content, date}) => {
-
-  return (
-    <>
-        {/* <div> 
-            <p><strong>{content}</strong></p>
-            <small><time>{date}</time></small>
-        </div> */}
-        <li> 
-            <p><strong>{content}</strong></p>
-            <small><time>{date}</time></small>
+const Note = ({ title, body }) => {
+    return (
+        <li>
+            <p><strong>{title}</strong></p>
+            <small><p>{body}</p></small>
         </li>
-        {/* <li> 
-            <p><strong>{content}</strong></p>
-            <small><time>{date}</time></small>
-        </li> */}
-  </>
-  )
-}
+    );
+};
 
 export default Note;
